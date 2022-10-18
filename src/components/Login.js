80cfd283270bca127cd756a39624a32cfa0e9054
omolaso <adextee001@gmail.com>
2022-10-18 03:26:43 +0100

    forms

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,33 @@
 import React, {useState} from 'react';
 import '../styles/Login.css';
-import { useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 const Login = () => {
+
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    validationSchema: Yup.object({
+      email: Yup.string()
+        .email('Invalid email address')
+        .required('Required'),
+      
+      password: Yup.string()
+      .required('Password is required')
+      .min(8, 'Password is too short. Minimum of 8 characters.')
+      .max(10, 'Password must be at most 10 characters')
+      .matches( /^.*(?=.{8,})((?=.*[!@#$%^&*()\-_=+{};:,<.>]){1})(?=.*\d)((?=.*[a-z]){1})((?=.*[A-Z]){1}).*$/, 
+        'Password must contain at least one number, one uppercase, one lowercase letter and a dynamic symbol.'),
+    }),
+    onSubmit: values => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const navigate = useNavigate();
@@ -22,20 +47,28 @@ const Login = () => {
               <label htmlFor="email" id='name'> Email: </label>
               <input 
                 type="email" 
+                id='email'
+                name='email'
                 value={email} 
                 placeholder='Enter your email' 
                 onChange={(e) => setEmail(e.target.value)}
-                required
-                />
+                {...formik.getFieldProps('email')}
+              />
+              {formik.touched.email && formik.errors.email ? (<div>{formik.errors.email}</div>) : null}
+
 
               <label htmlFor="password" id='name'> Password: </label>
               <input 
                 type="password" 
+                id='password'
+                name='password'
                 value={password} 
                 onChange={(e) => setPassword(e.target.value)} 
                 placeholder='Enter your password'
-                required
+                {...formik.getFieldProps('password')}
                 />
+              {formik.touched.password && formik.errors.password ? (<div>{formik.errors.password}</div>) : null}
+
               <br />
               <br />
               <div id='btn'>
